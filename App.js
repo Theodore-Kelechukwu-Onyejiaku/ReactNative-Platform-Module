eb695f69e72257e3d618c1208903bb20505909a9
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2023-03-05 14:16:36 +0100

    implement drawer-layout

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React, { useRef, useState } from "react";
+import {
+  Button,
+  DrawerLayoutAndroid,
+  Text,
+  StyleSheet,
+  View,
+  Platform,
+} from "react-native";
+import { Drawer } from "react-native-drawer-layout"
 
-export default function App() {
-  return (
+const App = () => {
+  const [open, setOpen] = React.useState(false);
+
+  // create navigation view for android
+  const navigationView = () => (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text >I'm the Drawer!</Text>
     </View>
   );
-}
+
+  return (
+    <>
+      {/* Select for os */}
+      {Platform.OS === "ios" ?
+        <Drawer
+          open={open}
+          onOpen={() => setOpen(true)}
+          onClose={() => setOpen(false)}
+          renderDrawerContent={() => {
+            return <View style={styles.container}>
+              <Text>I'm the Drawer!</Text>
+            </View>;
+          }}
+        >
+          <View style={styles.container}>
+            <Text>Swipe from the left (Drawerlayout for iOS)</Text>
+          </View>
+        </Drawer>
+        :
+        <DrawerLayoutAndroid
+          drawerWidth={300}
+          renderNavigationView={navigationView}>
+          <View style={styles.container}>
+            <Text style={styles.paragraph}>
+              Swipe from the left (Drawerlayout for Android)
+            </Text>
+          </View>
+        </DrawerLayoutAndroid>
+      }
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16,
+  },
+  paragraph: {
+    padding: 16,
+    fontSize: 15,
+    textAlign: 'center',
   },
 });
+
+export default App;
