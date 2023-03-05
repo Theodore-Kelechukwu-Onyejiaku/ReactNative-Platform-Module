f9d78dfaa1f06602a979ed025882c8e7f1746794
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2023-03-05 23:00:29 +0100

    remove unwanted modules

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { StyleSheet,Platform } from 'react-native';
 import { WebView } from "react-native-webview"
 
 export default function App() {
@@ -10,12 +9,3 @@ export default function App() {
       <WebView source={{ uri: "https://blog.openreplay.com/" }} geolocationEnabled={true} scalesPageToFit={true} domStorageEnabled={true} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
